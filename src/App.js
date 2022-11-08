1630deb3b3b06cf3adff89d6260e2d41a2907230
Ravi Dhingra <ravidhingrabkn@outlook.com>
2022-11-08 12:34:39 -0500

    initial release

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const time = new Date();
+  const day = time.toLocaleString("en-us", { weekday: "long"});
+  const morning = time.getHours() >= 6 && time.getHours() <= 11;
+  let dayMessage;
+
+// console.log(`${day}`);
+
+  if(day.toLowerCase() === "monday"){
+    dayMessage = `Happy ${day}`;
+  } else if (day.toLowerCase() === "tuesday"){
+    dayMessage = `${day}, four days to go`;
+  } else if (day.toLowerCase === "wednesday"){
+    dayMessage = `${day}, half way there`;
+  } else if (day.toLowerCase === "thursday"){
+    dayMessage = `${day}, start planning the weekend`;
+  }else if (day.toLowerCase === "friday"){
+    dayMessage = `Woo - hoo, the weekend is coming!`;
+  }else {
+    dayMessage = "Saty calm and keep having fun";
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{dayMessage}</h1>
+    
+    {morning ? <h2>Have you had breakfast yet?</h2> : ''}
     </div>
   );
 }
